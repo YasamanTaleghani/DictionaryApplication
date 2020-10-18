9685f8baa37c2aef4ff23fbf2b759e09ce2260e4
304yasaman <y.taleghani@yahoo.com>
2020-10-19 00:10:45 +0330

    Required packages are made.

--- app/src/main/java/com/example/dictionaryapplication/Controller/Activity/MainActivity.java
+++ app/src/main/java/com/example/dictionaryapplication/Controller/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.dictionaryapplication;
+package com.example.dictionaryapplication.Controller.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.dictionaryapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
